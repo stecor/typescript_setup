ce1fbe132dde75b939be2334f86f2c4d44f8ec8a
Stefano Corra <stefano.corra@hotmail.com>
2017-06-11 01:23:15 -0400

    third commit

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,37 @@
-var SweetSweetClass = (function () {
-    function SweetSweetClass() {
-        console.log("Even sweeter");
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Basil = (function () {
+    function Basil(name, StartColor) {
     }
-    return SweetSweetClass;
+    Basil.prototype.getColor = function () {
+        console.log(this.color);
+    };
+    return Basil;
 }());
-var basil = new SweetSweetClass();
+var world = new Basil("World", "green");
+var BasilGreen = (function (_super) {
+    __extends(BasilGreen, _super);
+    function BasilGreen(name, startColor) {
+        var _this = _super.call(this, name, startColor) || this;
+        _this.name = name;
+        _this.color = startColor;
+        return _this;
+    }
+    BasilGreen.prototype.setNewColor = function (newcolor) {
+        this.color = newcolor;
+        _super.prototype.getColor.call(this);
+    };
+    return BasilGreen;
+}(Basil));
+var basil3 = new BasilGreen("basil", "brite green");
+basil3.getColor();
+basil3.setNewColor("Red");
+basil3.getColor();
--- main.ts
+++ main.ts
@@ -1,6 +1,41 @@
-class SweetSweetClass {
-    constructor() {
-        console.log("Even sweeter")
-    }
+class Basil{
+  name:string;
+  color: string;
+  constructor(name:string, StartColor:string){
+    //console.log("Hello " + name)
+    //this.color = StartColor
+  }
+
+  getColor(){
+    console.log(this.color)
+  }
 }
-let basil = new SweetSweetClass()
+
+let world = new Basil("World","green")
+
+//let basil = new Basil("Basil", "bright green")
+
+//basil.getColor()
+//world.getColor()
+
+//console.log(basil.color)
+
+class BasilGreen extends Basil{
+
+  constructor(name:string,startColor:string){
+    super(name,startColor)
+    this.name = name
+    this.color = startColor
+  }
+
+  setNewColor(newcolor:string){
+    this.color = newcolor
+    super.getColor()
+  }
+}
+
+let basil3 = new BasilGreen("basil", "brite green")
+
+basil3.getColor()
+basil3.setNewColor("Red")
+basil3.getColor()
